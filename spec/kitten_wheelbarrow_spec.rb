08e1627614e1d5c01f7ba07ec1befa274908e773
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-21 15:33:26 +0000

    Done.

--- spec/kitten_wheelbarrow_spec.rb
+++ spec/kitten_wheelbarrow_spec.rb
@@ -1,6 +1,6 @@
 require './spec/spec_helper.rb'
 
-RSpec.describe "Kitten Wheelbarrow" do
+#RSpec.describe "Kitten Wheelbarrow" do
 
   it 'selects the image with a class of kitty-1' do
     expect(css_file_contents).to include('.kitty-1'), "Make sure you have a #ruby CSS selector that selects the image that has a class of kitty-1"
